9a87cc320ed23771e02c2e9d54c63b4d87262fa6
Brandon <bhmcarr@gmail.com>
2017-05-31 08:04:21 -0400

    begin racket movement

--- Assets/MoveRacket.cs
+++ Assets/MoveRacket.cs
@@ -3,14 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveRacket : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		
 	}
 }
